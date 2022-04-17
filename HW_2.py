dfaae0500eb1e75c16e3d0fa527f054542dee7c2
Артем <noob1488@icloud.com>
2022-04-17 21:17:28 +0300

    Update HW_2.py

--- HW_2.py
+++ HW_2.py
@@ -52,19 +52,25 @@ currency_convertor = item_2
 print(currency_convertor)
 if currency_convertor:
     currency_usd = usd_item
-    target_currency = eur_item
-    target_currency_amount = 50
+    target_currency = rub_item
+    target_currency_amount = 120000
     currency_result = 0
-    if target_currency =='eur':
+    if target_currency == "eur":
         currency_result = target_currency_amount / usd_eur_rate
         print(target_currency_amount, eur_item, "=", currency_result, usd_item)
-    elif target_currency == 'uah':
-        print(target_currency_amount, uah_item, "=",currency_result,uah_item )
-    elif target_currency == 'chf':
-        print(target_currency_amount, chf_item, "=", currency_result, chf_item)
-    elif target_currency == 'rub':
-        print(target_currency_amount, rub_item, "=", currency_result, rub_item)
+    elif target_currency == "uah":
+        currency_result = target_currency_amount / usd_uah_rate
+        print(target_currency_amount, uah_item, "=", currency_result, usd_item)
+    elif target_currency == "chf":
+        currency_result = target_currency_amount / usd_chf_rate
+        print(target_currency_amount, chf_item, "=", currency_result, usd_item)
+    elif target_currency == "rub":
+        currency_result = target_currency_amount / usd_rub_rate
+        print(target_currency_amount, rub_item, "=", currency_result, usd_item)
+    elif target_currency == "byn":
+        currency_result = target_currency_amount / usd_byn_rate
+        print(target_currency_amount, byn_item, "=", currency_result, usd_item)
     else:
         print("Unknow currency")
 else:
-    print('Переменная currency_convertor =', item_3)
+    print("Переменная currency_convertor =", item_3)
